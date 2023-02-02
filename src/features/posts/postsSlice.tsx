9291f7450fb89e219944e01b7221b5ffe2d3e573
raylu15742cs <raylucs15742@gmail.com>
2023-02-02 15:00:04 -0800

    update axos

--- src/features/posts/postsSlice.tsx
+++ src/features/posts/postsSlice.tsx
@@ -1,32 +1,27 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from 'date-fns'
+import axios from "axios";
 
-const initialState = [
-    {id: "1", 
-    title: 'Learning Redux Toolkit', 
-    content: "I've heard good things.",
-    date: sub(new Date(), {minutes:10}).toISOString(),
-    reactions: {
-        thumbsUp: 0,
-        wow: 0,
-        heart: 0,
-        rocket: 0,
-        coffee: 0
-    }
-    },
-    {id: "2", 
-    title: 'Slices...', 
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), {minutes:5}).toISOString(),
-    reactions: {
-        thumbsUp: 0,
-        wow: 0,
-        heart: 0,
-        rocket: 0,
-        coffee: 0
-    }
-    }
-]
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+const initialState: initialStates = {
+    posts: [],
+    status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+    error: null
+}
+
+type initialStates = {
+    posts: any[],
+    status: any,
+    error: any
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await axios.get(POSTS_URL)
+    return response.data
+})
+
 
 const postsSlice = createSlice({
     name: 'posts',
@@ -34,7 +29,7 @@ const postsSlice = createSlice({
     reducers: {
         postAdded: {
             reducer(state, action) {
-                state.push(action.payload)
+                state.posts.push(action.payload)
             },
             // @ts-ignore
             prepare( title, content, userId) {
@@ -58,17 +53,73 @@ const postsSlice = createSlice({
         },
         reactionAdded(state, action) {
             const { postId , reaction} = action.payload
-            const existingPost = state.find(post => post.id === postId)
+            const existingPost = state.posts.find(post => post.id === postId)
             if(existingPost) {
                 // @ts-ignore
                 existingPost.reactions[reaction]++
             }
         }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state,action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPosts.fulfilled, (state,action) => {
+                state.status = 'succeeded'
+                //adding date and reactions
+                let min = 1;
+                const loadedPosts = action.payload.map((post:any) => {
+                    post.date = sub(new Date(), {minutes: min++}).toISOString()
+                    post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                    }
+                    return post;
+                })
+
+                // Add any fetched post to array
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state,action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
     }
 })
 
-export const selectAllPosts = ( state:any ) => state.posts;
+export const selectAllPosts = ( state:any ) => state.posts.posts;
 
 export const { postAdded , reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
+
+// const initialState = [
+//     {id: "1", 
+//     title: 'Learning Redux Toolkit', 
+//     content: "I've heard good things.",
+//     date: sub(new Date(), {minutes:10}).toISOString(),
+//     reactions: {
+//         thumbsUp: 0,
+//         wow: 0,
+//         heart: 0,
+//         rocket: 0,
+//         coffee: 0
+//     }
+//     },
+//     {id: "2", 
+//     title: 'Slices...', 
+//     content: "The more I say slice, the more I want pizza.",
+//     date: sub(new Date(), {minutes:5}).toISOString(),
+//     reactions: {
+//         thumbsUp: 0,
+//         wow: 0,
+//         heart: 0,
+//         rocket: 0,
+//         coffee: 0
+//     }
+//     }
+// ]
